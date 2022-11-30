7bc304aff83818fda39976995486cb142ce86c5a
remiCzn <cazinremi2@gmail.com>
2022-11-30 17:35:45 +0100

    Cleans + port

--- postfix/start.sh
+++ postfix/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-cat etc/mailname | envsubst '$HOSTNAME' > etc/mainame
-cat etc/postfix/main.cf | envsubst '$HOSTNAME' > etc/postfix/main.cf
+replace_env_in_file () {
+  cat $1 | envsubst "`printf '${%s} ' $(sh -c "env|cut -d'=' -f1")`" > temp
+  mv temp $1
+}
+
+replace_env_in_file etc/mailname
+replace_env_in_file etc/postfix/main.cf
 service apache2 restart
 service postfix restart
 /bin/bash
