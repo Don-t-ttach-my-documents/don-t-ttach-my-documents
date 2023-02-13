9eabfcbcac4347b66d0713fc61975e511a14f845
remiCzn <cazinremi2@gmail.com>
2023-02-13 14:24:51 +0100

    change

--- archive-cli/archive_data.sh
+++ archive-cli/archive_data.sh
@@ -1,2 +1,2 @@
-mc alias set filestorage http://minio:9000 remi_czn password
-mc rm filestorage -older-than 30d --recursive --force --dangerous --fake
+mc alias set filestorage http://minio:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+mc rm filestorage -older-than $DELAY --recursive --force --dangerous --fake
